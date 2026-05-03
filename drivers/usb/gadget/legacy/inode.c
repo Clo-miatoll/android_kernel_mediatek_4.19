79d5a130e9a75a48311063e7223cc801ab448b31
Ben Grisdale <bengris32@protonmail.ch>
2026-05-04 00:35:48 +0100

    Revert "[ALPS07217346] UPSTREAM: usb: gadget: dont release an existing dev->buf"
    This reverts commit ec1a8eedbadc4fbb08d119119d5f05e530483811.
    
    Change-Id: I50cff083c0499c690847223189465bb60be9d422
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- drivers/usb/gadget/legacy/inode.c
+++ drivers/usb/gadget/legacy/inode.c
@@ -1828,9 +1828,8 @@ dev_config (struct file *fd, const char __user *buf, size_t len, loff_t *ptr)
 	spin_lock_irq (&dev->lock);
 	value = -EINVAL;
 	if (dev->buf) {
-		spin_unlock_irq(&dev->lock);
 		kfree(kbuf);
-		return value;
+		goto fail;
 	}
 	dev->buf = kbuf;
 
