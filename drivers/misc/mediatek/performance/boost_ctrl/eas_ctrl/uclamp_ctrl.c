6246cefc61ef4c9577af9fc22eb4c568fc02d3d5
Ben Grisdale <bengris32@protonmail.ch>
2026-05-25 21:28:05 +0100

    drivers: mediatek: eas_ctrl: Fix build error when schedtune is disabled
    Change-Id: I793958c5883601c159c94e00502cd67f28156c3e
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/uclamp_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/uclamp_ctrl.c
@@ -1216,10 +1216,13 @@ PROC_FOPS_RW(perfmgr_log);
 int uclamp_ctrl_init(struct proc_dir_entry *parent)
 {
 #ifdef CONFIG_MTK_SCHED_EXTENSION
-	int i, ret = 0;
+	int ret = 0;
 	size_t idx;
 #if defined(CONFIG_UCLAMP_TASK_GROUP) && defined(CONFIG_SCHED_TUNE)
 	int j;
+#endif
+#if defined(MTK_K14_EAS_BOOST) || (defined(CONFIG_UCLAMP_TASK_GROUP) && defined(CONFIG_SCHED_TUNE))
+	int i;
 #endif
 	struct pentry {
 		const char *name;
