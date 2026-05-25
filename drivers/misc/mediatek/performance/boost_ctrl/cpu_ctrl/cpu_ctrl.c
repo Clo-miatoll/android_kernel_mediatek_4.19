c465e83f64933497cdb052b75893be860c099b30
Ben Grisdale <bengris32@protonmail.ch>
2026-05-25 23:29:14 +0100

    Revert "[ALPS06518751] power: kernel-4.19 migration"
    This reverts commit 575d472076e80c95e7325b69f143dfe44f1a09cb.
    
    Change-Id: I8b64fb28c8fdabd8a9d789f4a596d2df511e59fe
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- drivers/misc/mediatek/performance/boost_ctrl/cpu_ctrl/cpu_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/cpu_ctrl/cpu_ctrl.c
@@ -10,7 +10,6 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
-#include <linux/cpufreq.h>
 
 #include <mt-plat/cpu_ctrl.h>
 #include <mt-plat/mtk_ppm_api.h>
@@ -28,10 +27,7 @@
 
 #include <linux/io.h>
 
-#define MAX_CLUSTER_COUNT 3
-
 static struct mutex boost_freq;
-static struct mutex isolate_lock;
 static struct ppm_limit_data *current_freq;
 static struct ppm_limit_data *freq_set[CPU_MAX_KIR];
 static int log_enable;
@@ -43,6 +39,7 @@ static int cfp_init_ret;
 
 int powerhal_tid;
 
+#ifdef MTK_K14_CPU_BOOST
 #include "eas_ctrl_plat.h"
 
 #define MAX_CLUSTER_COUNT 3
@@ -55,12 +52,13 @@ struct cluster_data {
 };
 static int *cpu_isolation[CPU_ISO_MAX_KIR];
 
+#ifdef CONFIG_MTK_CORE_CTL
 static struct cluster_data core_set[CPU_ISO_MAX_KIR][MAX_CLUSTER_COUNT];
 static struct cluster_data default_core_set[MAX_CLUSTER_COUNT];
 static int isolation_used[CPU_ISO_MAX_KIR];
-static int all_cpu_deisolated;
-static int isBooting;
+#endif
 
+#endif
 
 /*******************************************/
 int update_userlimit_cpu_freq(int kicker, int num_cluster
@@ -275,7 +273,7 @@ static int perfmgr_perfserv_freq_proc_show(struct seq_file *m, void *v)
 static ssize_t perfmgr_boot_freq_proc_write(struct file *filp,
 		const char __user *ubuf, size_t cnt, loff_t *pos)
 {
-	int i = 0, data, cid;
+	int i = 0, data;
 	struct cpu_ctrl_data *freq_limit;
 	unsigned int arg_num = perfmgr_clusters * 2; /* for min and max */
 	char *tok, *tmp;
@@ -316,21 +314,6 @@ static ssize_t perfmgr_boot_freq_proc_write(struct file *filp,
 			i++;
 		}
 	}
-
-	isBooting = 0;
-	for_each_perfmgr_clusters(cid)
-		if (freq_limit[cid].min != -1 || freq_limit[cid].max != -1) {
-			isBooting = 1;
-#ifdef CONFIG_MTK_CORE_CTL
-			core_ctl_set_boost(1);
-#endif
-			break;
-		}
-
-#ifdef CONFIG_MTK_CORE_CTL
-	if (!isBooting)
-		core_ctl_set_boost(all_cpu_deisolated);
-#endif
 
 	if (i < arg_num)
 		pr_debug("@%s: number of arguments < %d!\n",
@@ -401,6 +384,7 @@ static int perfmgr_perfmgr_log_proc_show(struct seq_file *m, void *v)
 	return 0;
 }
 
+#ifdef MTK_K14_CPU_BOOST
 void update_isolation_cpu(int kicker, int enable, int cpu)
 {
 	int i, final = -1;
@@ -558,13 +542,6 @@ static ssize_t perfmgr_perfserv_all_cpu_deisolated_proc_write
 
 	all_cpu_deisolated = (data > 0) ? 1 : 0;
 
-#ifdef CONFIG_MTK_CORE_CTL
-	if (isBooting)
-		core_ctl_set_boost(1);
-	else
-		core_ctl_set_boost(all_cpu_deisolated);
-#endif
-
 	return cnt;
 }
 
@@ -581,9 +558,9 @@ PROC_FOPS_RW(perfserv_freq);
 PROC_FOPS_RW(boot_freq);
 PROC_FOPS_RO(current_freq);
 PROC_FOPS_RW(perfmgr_log);
+#ifdef MTK_K14_CPU_BOOST
 PROC_FOPS_RW(perfserv_iso_cpu);
-PROC_FOPS_RW(perfserv_core);
-PROC_FOPS_RW(perfserv_all_cpu_deisolated);
+#endif
 
 /************************************************/
 int cpu_ctrl_init(struct proc_dir_entry *parent)
@@ -602,12 +579,11 @@ int cpu_ctrl_init(struct proc_dir_entry *parent)
 		PROC_ENTRY(boot_freq),
 		PROC_ENTRY(current_freq),
 		PROC_ENTRY(perfmgr_log),
+#ifdef MTK_K14_CPU_BOOST
 		PROC_ENTRY(perfserv_iso_cpu),
-		PROC_ENTRY(perfserv_core),
-		PROC_ENTRY(perfserv_all_cpu_deisolated),
+#endif
 	};
 	mutex_init(&boost_freq);
-	mutex_init(&isolate_lock);
 
 	boost_dir = proc_mkdir("cpu_ctrl", parent);
 
@@ -657,40 +633,6 @@ int cpu_ctrl_init(struct proc_dir_entry *parent)
 			freq_set[i][j].max = -1;
 		}
 	}
-
-	num_cpu = num_possible_cpus();
-	for (i = 0; i < CPU_ISO_MAX_KIR; i++) {
-		cpu_isolation[i] = kcalloc(num_cpu, sizeof(int),
-					GFP_KERNEL);
-		for (j = 0; j < num_cpu; j++)
-			cpu_isolation[i][j] = -1;
-		isolation_used[i] = 0;
-
-		for_each_perfmgr_clusters(j) {
-			core_set[i][j].core_min = -1;
-			core_set[i][j].core_max = -1;
-		}
-	}
-
-	perfserv_isolation_cpu = 0;
-	all_cpu_deisolated = 0;
-
-	// sync with core_ctl
-	for_each_perfmgr_clusters(i) {
-		if (i == 0) {
-			default_core_set[i].core_min = 4;
-			default_core_set[i].core_max = 4;
-		} else if (i == 1) {
-			default_core_set[i].core_min = 2;
-			default_core_set[i].core_max = 3;
-		} else if (i == 2) {
-			default_core_set[i].core_min = 0;
-			default_core_set[i].core_max = 1;
-		}
-	}
-
-	isBooting = 1;
-
 out:
 	return ret;
 
@@ -704,8 +646,6 @@ void cpu_ctrl_exit(void)
 	kfree(policy_mask);
 	for (i = 0; i < CPU_MAX_KIR; i++)
 		kfree(freq_set[i]);
-	for (i = 0; i < CPU_ISO_MAX_KIR; i++)
-		kfree(cpu_isolation[i]);
 
 #ifdef CONFIG_MTK_CPU_CTRL_CFP
 	if (!cfp_init_ret)
