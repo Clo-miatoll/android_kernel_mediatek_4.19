b8d6bacbf6a01f80e7fefab5cff427b419683b61
Ben Grisdale <bengris32@protonmail.ch>
2026-05-26 16:31:19 +0100

    sched/rt: Remove MTK prints
    Change-Id: I52de3dee1deb8d56e975c5340e726d29976a8b7e
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -899,11 +899,6 @@ static int do_sched_rt_period_timer(struct rt_bandwidth *rt_b, int overrun)
 				rt_rq->rt_throttled = 0;
 				enqueue = 1;
 
-#ifdef CONFIG_MTK_SCHED_EXTENSION
-				printk_deferred("[name:rt&]sched: RT throttling inactivated cpu=%d\n",
-						i);
-#endif
-
 				/*
 				 * When we're idle and a woken (rt) task is
 				 * throttled check_preempt_curr() will set
@@ -972,11 +967,7 @@ static int sched_rt_runtime_exceeded(struct rt_rq *rt_rq)
 		 */
 		if (likely(rt_b->rt_runtime)) {
 			rt_rq->rt_throttled = 1;
-#ifdef CONFIG_MTK_SCHED_EXTENSION
-			printk_deferred("[name:rt&]sched: RT throttling activated\n");
-#else
 			printk_deferred_once("sched: RT throttling activated\n");
-#endif
 		} else {
 			/*
 			 * In case we did anyway, make it go away,
