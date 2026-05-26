f29eadbc56aaa257248124cdfd4d5b1f1dc537dd
Ben Grisdale <bengris32@protonmail.ch>
2026-05-26 14:04:27 +0100

    Revert "[ALPS06414038] task-turbo: scheduler tuning for task turbo"
    This reverts commit ae3cae9248a32cca3f7b2db6d29830d333064a08.
    
    Change-Id: I28ff6e297ca0349056246ee3e962115f06994f6b
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -22,9 +22,6 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/sched.h>
-#ifdef CONFIG_MTK_TASK_TURBO
-#include <mt-plat/turbo_common.h>
-#endif
 #ifdef CONFIG_MTK_QOS_FRAMEWORK
 #include <mt-plat/mtk_qos_prefetch_common.h>
 #endif /* CONFIG_MTK_QOS_FRAMEWORK */
@@ -3007,18 +3004,9 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	 * Make sure we do not leak PI boosting priority to the child.
 	 */
 	p->prio = current->normal_prio;
-#ifdef CONFIG_MTK_TASK_TURBO
-	if (unlikely(is_turbo_task(current))) {
-		if (task_has_dl_policy(p) || task_has_rt_policy(p))
-			p->static_prio = NICE_TO_PRIO(current->nice_backup);
-		else {
-			p->static_prio = NICE_TO_PRIO(current->nice_backup);
-			p->prio = p->normal_prio = p->static_prio;
-			set_load_weight(p, false);
-		}
-	}
-#endif
+
 	uclamp_fork(p);
+
 	/*
 	 * Revert to default priority/policy on fork if requested.
 	 */
@@ -3045,13 +3033,8 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 		return -EAGAIN;
 	else if (rt_prio(p->prio))
 		p->sched_class = &rt_sched_class;
-	else {
+	else
 		p->sched_class = &fair_sched_class;
-#ifdef CONFIG_MTK_TASK_TURBO
-		/* prio and backup should be aligned */
-		p->nice_backup = PRIO_TO_NICE(p->prio);
-#endif
-	}
 
 	init_entity_runnable_average(&p->se);
 
@@ -4522,20 +4505,6 @@ void rt_mutex_setprio(struct task_struct *p, struct task_struct *pi_task)
 	struct rq_flags rf;
 	struct rq *rq;
 
-#ifdef CONFIG_MTK_TASK_TURBO
-	/* if rt boost, recover prio with backup */
-	if (unlikely(is_turbo_task(p))) {
-		if (!dl_prio(p->prio) && !rt_prio(p->prio)) {
-			int backup = p->nice_backup;
-
-			if (backup >= MIN_NICE && backup <= MAX_NICE) {
-				p->static_prio = NICE_TO_PRIO(backup);
-				p->prio = p->normal_prio = __normal_prio(p);
-				set_load_weight(p, false);
-			}
-		}
-	}
-#endif
 	/* XXX used to be waiter->prio, not waiter->task->prio */
 	prio = __rt_effective_prio(pi_task, p->normal_prio);
 
@@ -4652,10 +4621,6 @@ static inline int rt_effective_prio(struct task_struct *p, int prio)
 }
 #endif
 
-#ifdef CONFIG_MTK_TASK_TURBO
-#define task_turbo_nice(nice) (nice == 0xbeef || nice == 0xbeee)
-#endif
-
 void set_user_nice(struct task_struct *p, long nice)
 {
 	bool queued, running;
@@ -4663,32 +4628,14 @@ void set_user_nice(struct task_struct *p, long nice)
 	struct rq_flags rf;
 	struct rq *rq;
 
-#ifdef CONFIG_MTK_TASK_TURBO
-	if ((nice < MIN_NICE || nice > MAX_NICE) && !task_turbo_nice(nice))
-		return;
-#else
 	if (task_nice(p) == nice || nice < MIN_NICE || nice > MAX_NICE)
 		return;
-#endif
 	/*
 	 * We have to be careful, if called from sys_setpriority(),
 	 * the task might be in the middle of scheduling on another CPU.
 	 */
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
-
-#ifdef CONFIG_MTK_TASK_TURBO
-	/* for general use, backup it */
-	if (!task_turbo_nice(nice))
-		p->nice_backup = nice;
-
-	if (is_turbo_task(p))
-		nice = rlimit_to_nice(task_rlimit(p, RLIMIT_NICE));
-	else
-		nice = p->nice_backup;
-
-	trace_sched_set_user_nice(p, nice, is_turbo_task(p));
-#endif
 
 	/*
 	 * The RT priorities are set via sched_setscheduler(), but we still
@@ -4910,15 +4857,8 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 		p->sched_class = &dl_sched_class;
 	else if (rt_prio(p->prio))
 		p->sched_class = &rt_sched_class;
-#ifdef CONFIG_MTK_TASK_TURBO
-	else {
-		p->sched_class = &fair_sched_class;
-		p->nice_backup = PRIO_TO_NICE(p->prio);
-	}
-#else
 	else
 		p->sched_class = &fair_sched_class;
-#endif
 }
 
 /*
