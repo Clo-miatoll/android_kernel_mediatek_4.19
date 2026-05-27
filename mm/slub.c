08fee90f38b815a2e04ee262b10e1a964c17f625
Ben Grisdale <bengris32@protonmail.ch>
2026-05-27 23:45:59 +0100

    Revert "FROMLIST: mm/slub: use WARN_ON() for some slab errors"
    This reverts commit d8a4196a6b45c55cbb599387d06cb4b3df39ece7.
    
    Change-Id: Ifd576aa00032cb112933d2796ec3e2ce8b194dc4
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- mm/slub.c
+++ mm/slub.c
@@ -705,7 +705,7 @@ static void print_trailer(struct kmem_cache *s, struct page *page, u8 *p)
 		print_section(KERN_ERR, "Padding ", p + off,
 			      size_from_object(s) - off);
 
-	WARN_ON(1);
+	dump_stack();
 }
 
 void object_err(struct kmem_cache *s, struct page *page,
@@ -726,7 +726,7 @@ static __printf(3, 4) void slab_err(struct kmem_cache *s, struct page *page,
 	va_end(args);
 	slab_bug(s, "%s", buf);
 	print_page_info(page);
-	WARN_ON(1);
+	dump_stack();
 }
 
 static void init_object(struct kmem_cache *s, void *object, u8 val)
@@ -1023,7 +1023,7 @@ static void trace(struct kmem_cache *s, struct page *page, void *object,
 			print_section(KERN_INFO, "Object ", (void *)object,
 					s->object_size);
 
-		WARN_ON(1);
+		dump_stack();
 	}
 }
 
